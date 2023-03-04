c190955ef580090b12bfd2299b2387500301f808
chidq-at-967132174866 <chidq-at-967132174866>
2023-03-04 21:11:42 +0700

    create new file

--- learn_java_io/src/CreateFileExample.java
+++ learn_java_io/src/CreateFileExample.java
@@ -1,2 +1,17 @@
+import java.io.File;
+import java.io.IOException;
+
 public class CreateFileExample {
+    public static void main(String[] args) {
+        File file = new File("D:\\learn_io_java\\newfile.txt");
+        try {
+            if(file.createNewFile()) {
+                System.out.println("File is created");
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
